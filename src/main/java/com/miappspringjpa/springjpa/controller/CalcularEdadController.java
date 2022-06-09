d7fb53691a11bd7aca486bd7aa0e6a30c8031dfa
maxpipoka <martincardozo@gmail.com>
2022-06-09 11:50:24 -0300

    Merge branch 'main' of https://github.com/maxpipoka/arg-prog-spring-jpa

--- src/main/java/com/miappspringjpa/springjpa/controller/CalcularEdadController.java
+++ src/main/java/com/miappspringjpa/springjpa/controller/CalcularEdadController.java
@@ -1,10 +1,28 @@
 package com.miappspringjpa.springjpa.controller;
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.miappspringjpa.springjpa.service.ICalcularEdadService;
+
+
 
 @RestController
 public class CalcularEdadController {
 
-    
+    @Autowired
+    private ICalcularEdadService calcularEdadService;
+
+    @GetMapping("/calcularedad/{dia}/{mes}/{año}")
+    public String calculoDeEdad(@PathVariable int dia,
+                                @PathVariable int mes,
+                                @PathVariable int año){
+        Date fNacimiento = new Date(año, mes, dia);
+        return calcularEdadService.calcularEdad(fNacimiento);
+    }
     
 }
